e210f050bc2770c0b921b39d33a0d82bcb0ed876
TeddyAdams <80841726+IrmaAdams@users.noreply.github.com>
2023-12-07 14:30:30 +0000

    ECS Folder structure

---
+++ src/Systems/MovementSystem.h
@@ -0,0 +1,16 @@
+#ifndef MOVEMENTSYSTEM_H
+#define MOVEMENTSYSTEM_H
+
+class MovementSystem : public System {
+public:
+	MovementSystem() {
+		// entities with transform components
+
+	}
+
+	void Update() {
+		//for (auto entity : GetEntities()) {}
+	}
+};
+
+#endif
